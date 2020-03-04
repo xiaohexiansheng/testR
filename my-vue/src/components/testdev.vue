f23756d580ffbc12b55ac801c11dabf0df1a108c
何文彬 <309003192@qq.com>
2020-03-04 18:27:13 +0800

    主干master

--- my-vue/src/components/testdev.vue
+++ my-vue/src/components/testdev.vue
@@ -1,6 +1,16 @@
 <template>
 	<div class="dragbox">
-		<p @click="goTsetDev">测试第一次修改提交</p>
+		<el-row :gutter="20">
+		  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+		  <el-col :span="6" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+		</el-row>
+		<el-row :gutter="20">
+		  <el-col :span="6" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+		  <el-col :span="6" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+		</el-row>
+		<el-row :gutter="20">
+		  <el-col :span="12" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+		</el-row>
 	</div>
 </template>
 
@@ -13,7 +23,7 @@
 		},
 		methods: {
       goTsetDev(){
-        this.$router.push('/HelloWorld')
+        this.$router.push('/')
       }
 		},
 		mounted() {
@@ -31,4 +41,30 @@
   line-height: 40px;
   text-align: center;
 }
+.el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
