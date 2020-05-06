f859a6f68c93bfe4ed392dcf198414a0a3599651
hewb <hewb@hvgroup.com.cn>
2020-05-06 09:37:05 +0800

    222222

--- my-vue/src/components/HelloWorld.vue
+++ my-vue/src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
 	<div class="dragbox">
-		<p @click="goTsetDev">我是一个testdev</p>
-		<div style="color: #000;">111122</div>
+		<p @click="goTsetDev">我是一个testdev11111111222222222222221</p>
 	</div>
 </template>
 
